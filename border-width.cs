3704d6ec53c4809d700df1841c2d66fd8f90b1e9
davidfffelix <davidffelix@hotmail.com>
2022-01-13 11:39:40 -0300

    Border Width

--- border-width.cs
+++ border-width.cs
@@ -4,3 +4,10 @@ p {
     border-width: trick;
     border-width: 15px;
 }
+
+h1 {
+    border-top-width: 2px;
+    border-right-width: 2px;
+    border-bottom-width: 2px;
+    border-left-width: 2px;
+}
